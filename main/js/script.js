9db7376f5d213f74a463b04cd5247a17aa4c4676
Lisa Welling <lisa.welling@hva.nl>
2025-10-11 20:33:06 +0200

    ff terug naar eerdere versie

--- main/js/script.js
+++ main/js/script.js
@@ -36,3 +36,20 @@ AFRAME.registerComponent("pinch-detector", {
         if (this.debugText) this.debugText.setAttribute("value", msg);
     },
 });
+
+
+
+
+AFRAME.registerComponent('pinch-logger', {
+    schema: { hand: { default: 'left' } },
+    init: function () {
+        const statusText = document.querySelector('#statusText');
+        this.el.addEventListener('pinchstarted', () => {
+            statusText.setAttribute('text', 'value', `${this.data.hand} hand pinch started`);
+        });
+        this.el.addEventListener('pinchended', () => {
+            statusText.setAttribute('text', 'value', `${this.data.hand} hand pinch ended`);
+        });
+    }
+});
+
