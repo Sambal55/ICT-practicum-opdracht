50c3b98c05b3ad6fa2932ca89ef54ceb518bed0d
Lisa Welling <lisa.welling@hva.nl>
2025-10-13 15:57:35 +0200

    grabber van stackOverflow

--- main/js/script.js
+++ main/js/script.js
@@ -27,3 +27,24 @@ AFRAME.registerComponent("input-listen", {
         });
     }
 });
+
+
+AFRAME.registerComponent('grabber', {
+    init: function () {
+        this.grabbed = null;
+
+    },
+    events: {
+        gripdown: function(evt) {
+            if (evt.currentTarget.components['raycaster'].intersections.length>0) {
+                this.grabbed = evt.currentTarget.components['raycaster'].intersections[0].object.el;
+                evt.currentTarget.object3D.attach(this.grabbed.object3D);
+            }
+        }, gripup: function(evt) {
+            if (this.grabbed) {
+                this.el.sceneEl.object3D.attach(this.grabbed.object3D);
+                this.grabbed = null;
+            }
+        }
+    }
+});
