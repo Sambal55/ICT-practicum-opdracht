118a803398c2c07723121c5fd38e58e66d89500f
Lisa Welling <lisa.welling@hva.nl>
2025-10-13 14:10:22 +0200

    grab object in js

--- main/js/script.js
+++ main/js/script.js
@@ -4,6 +4,28 @@ AFRAME.registerComponent("input-listen", {
         this.el.addEventListener("triggerdown", (e) => {
             console.log("triggerdown", e.target.id);
         });
+
+        this.el.grab = false;
+
+        //Called when trigger is pressed
+        //squeeze button in WebXR debugger
+        this.el.addEventListener("triggerdown", function (e) {
+            console.log("triggerdown", e.target.id);
+
+            //Setting grab flag as true.
+            this.grab = true;
+        });
+
+        //Called when trigger is release
+        //squeeze button in WebXR debugger
+        this.el.addEventListener("triggerup", function (e) {
+            console.log("triggerup", e.target.id);
+
+            //Setting grab flag as false.
+            this.grab = false;
+
+        });
+
         // veranderen naar purp if not purp already
         this.el.addEventListener("collide", (e) => {
             const hitEl = e.detail.body.el;
@@ -13,6 +35,15 @@ AFRAME.registerComponent("input-listen", {
                 console.log("collide with", hitEl.id || hitEl.className);
                 hitEl.setAttribute("color", "purple");
             }
+            if( this.grab === true){
+                console.log("hit");
+                console.log(hitEl.attributes.color);
+                // maak t blokkie geel als je m oppakt
+                hitEl.setAttribute('color', 'yellow')
+                console.log(hitEl.attributes.color);
+            }
+
         });
     }
+
 });
