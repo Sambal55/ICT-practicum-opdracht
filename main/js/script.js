0738b066f8b09a8e9a4f9abb0f13d347170bcf99
Lisa Welling <lisa.welling@hva.nl>
2025-10-13 16:26:50 +0200

    debugger

--- main/js/script.js
+++ main/js/script.js
@@ -1,50 +1,51 @@
-AFRAME.registerComponent("input-listen", {
-    init: function () {
-        let lastHit = null;
-
-        // Wordt geactiveerd zodra raycaster iets raakt
-        this.el.addEventListener("raycaster-intersection", (e) => {
-            const hits = e.detail.els;
-            if (hits && hits.length > 0) {
-                lastHit = hits[0];
-                console.log("Raycaster raakt:", lastHit.id || lastHit.className);
-            }
-        });
-
-        // Wordt geactiveerd zodra raycaster niets meer raakt
-        this.el.addEventListener("raycaster-intersection-cleared", () => {
-            lastHit = null;
-        });
-
-        // Trigger indrukken
-        this.el.addEventListener("triggerdown", () => {
-            if (lastHit) {
-                console.log("Grijpt:", lastHit.id || lastHit.className);
-                lastHit.setAttribute("color", "yellow");
-            } else {
-                console.log("Geen object geraakt bij triggerdown");
-            }
-        });
-    }
-});
-
-
 AFRAME.registerComponent('grabber', {
     init: function () {
         this.grabbed = null;
+        this.lastHit = null;
 
-    },
-    events: {
-        gripdown: function(evt) {
-            if (evt.currentTarget.components['raycaster'].intersections.length>0) {
-                this.grabbed = evt.currentTarget.components['raycaster'].intersections[0].object.el;
-                evt.currentTarget.object3D.attach(this.grabbed.object3D);
+        // Debugger ophalen
+        const debugEl = document.querySelector("#debugDisplay");
+
+        // Raycaster-intersection opslaan
+        this.el.addEventListener("raycaster-intersection", (e) => {
+            const hits = e.detail.els;
+            if (hits && hits.length > 0) {
+                this.lastHit = hits[0];
             }
-        }, gripup: function(evt) {
+        });
+
+        this.el.addEventListener("raycaster-intersection-cleared", () => {
+            this.lastHit = null;
+        });
+
+        // Grip indrukken
+        this.el.addEventListener("gripdown", () => {
+            if (debugEl && debugEl.components["vr-debugger"]) {
+                debugEl.components["vr-debugger"].log("✊ Grip ingedrukt");
+            }
+
+            if (this.lastHit && this.lastHit.classList.contains("grabbable")) {
+                this.grabbed = this.lastHit;
+                this.el.object3D.attach(this.grabbed.object3D);
+                if (debugEl && debugEl.components["vr-debugger"]) {
+                    debugEl.components["vr-debugger"].log("✅ Gegrepen: " + (this.grabbed.id || this.grabbed.className));
+                }
+            } else {
+                if (debugEl && debugEl.components["vr-debugger"]) {
+                    debugEl.components["vr-debugger"].log("⚠️ Geen grabbable object geraakt bij gripdown");
+                }
+            }
+        });
+
+        // Grip loslaten
+        this.el.addEventListener("gripup", () => {
             if (this.grabbed) {
                 this.el.sceneEl.object3D.attach(this.grabbed.object3D);
+                if (debugEl && debugEl.components["vr-debugger"]) {
+                    debugEl.components["vr-debugger"].log("🛑 Losgelaten");
+                }
                 this.grabbed = null;
             }
-        }
+        });
     }
 });
